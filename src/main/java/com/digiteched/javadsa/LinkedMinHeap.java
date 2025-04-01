676670aacd287fff851c46dec6bf8af54a008c76
Michael Lebrun <Lebrunm231@mytru.ca>
2025-04-01 15:12:21 -0700

    add logic should work, added node reference to node no idea how to find where to add though so I can't test it

--- src/main/java/com/digiteched/javadsa/LinkedMinHeap.java
+++ src/main/java/com/digiteched/javadsa/LinkedMinHeap.java
@@ -11,6 +11,8 @@ public class LinkedMinHeap<T extends Comparable<T>> implements IMinHeap<T> {
             this.data = data;
         }
 
+        LinkedNode parent;
+
         LinkedNode left;
 
         LinkedNode right;
@@ -24,6 +26,7 @@ public class LinkedMinHeap<T extends Comparable<T>> implements IMinHeap<T> {
     }
     private int count = 0;
     private LinkedNode root;
+    private LinkedNode lastNodeAdded;
 
     @Override
     public int size() {
@@ -42,9 +45,21 @@ public class LinkedMinHeap<T extends Comparable<T>> implements IMinHeap<T> {
         if(isEmpty()){
             root = nodeToAdd;
             count++;
+            lastNodeAdded = nodeToAdd;
             return;
         }
-        throw new UnsupportedOperationException("Unimplemented method 'add'");
+        LinkedNode nodeToAddTo = nodeToAddTo();
+        if (nodeToAddTo.left == null){
+            nodeToAddTo.left = nodeToAdd;
+        }
+        else if(nodeToAddTo.right == null){
+            nodeToAddTo.right = nodeToAdd;
+        } else {
+            throw new UnsupportedOperationException("How get here?");
+        }
+        nodeToAdd.parent = nodeToAddTo;
+        count++;
+        bubbleUp(nodeToAddTo);
     }
 
     @Override
@@ -58,4 +73,24 @@ public class LinkedMinHeap<T extends Comparable<T>> implements IMinHeap<T> {
       return root.data;
     }
 
+    private LinkedNode nodeToAddTo(){
+        return nodeToAddTo(lastNodeAdded);
+    }
+
+    private LinkedNode nodeToAddTo(LinkedNode lastNodeChecked){
+        if(lastNodeChecked == root){
+            //we have either gone all the way up and need to go left, or we started here
+            if(lastNodeChecked.left == null || lastNodeChecked.right == null){
+                return root; //we are adding to the root
+            }
+        }
+        LinkedNode parent = lastNodeChecked.parent;
+        if(parent.right == lastNodeChecked){
+           return nodeToAddTo(parent);
+    }
+throw new UnsupportedOperationException("I have no idea what I'm doing");
+}
+private void bubbleUp(LinkedNode node){
+throw new UnsupportedOperationException("Bubble up no workie yet");
+}
 }
