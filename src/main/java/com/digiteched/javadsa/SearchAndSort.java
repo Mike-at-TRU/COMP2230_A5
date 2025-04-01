e213b6e283506188d9173b9fc37ab7a753d7d480
Michael Lebrun <Lebrunm231@mytru.ca>
2025-04-01 13:38:26 -0700

    e2 part b done?

--- src/main/java/com/digiteched/javadsa/SearchAndSort.java
+++ src/main/java/com/digiteched/javadsa/SearchAndSort.java
@@ -1,6 +1,8 @@
 package com.digiteched.javadsa;
 
 public class SearchAndSort {
+    // "Add an additional static method called `heapSort` to the `SearchAndSort`
+    // class." done? you didn't say what you wanted it to do...
     public static <T extends Comparable<T>> void heapSort(T[] items) {
         throw new UnsupportedOperationException();
     }
