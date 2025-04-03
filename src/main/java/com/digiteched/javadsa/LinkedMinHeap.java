e2cc451bf686ab96c7045d97170a03c295beb760
Michael Lebrun <Lebrunm231@mytru.ca>
2025-04-02 19:29:23 -0700

    bubble up working, no I just need to bubble down when removed

--- src/main/java/com/digiteched/javadsa/LinkedMinHeap.java
+++ src/main/java/com/digiteched/javadsa/LinkedMinHeap.java
@@ -59,13 +59,57 @@ public class LinkedMinHeap<T extends Comparable<T>> implements IMinHeap<T> {
         }
         nodeToAdd.parent = nodeToAddTo;
         count++;
-        bubbleUp(nodeToAddTo);
+        bubbleUp(nodeToAdd);
     }
 
     @Override
     public T removeMin() {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeMin'");
+        T temp = root.data;
+        lastNodeAdded.left = root.left;
+        lastNodeAdded.right = root.right;
+        root = lastNodeAdded;
+        if(root.left != null){
+            root.left.parent = root;
+        }
+        if(root.right != null){
+            root.right.parent = root;
+        }
+        bubbleDown(root);
+        return temp;
+    }
+
+    private void bubbleDown(LinkedNode node) {
+        if(node.left == null || node.right == null){ // we know we are at the bottom, either one child left or no child
+            if(node.right != null){ //this would mean left is null and I fucked something up
+                throw new UnsupportedOperationException("I think I fucked up");
+            }
+
+            if(node.left != null){
+                if(node.compareTo(node.left) > 0){
+                    // we know left shouldn't have children since we only have one
+                    LinkedNode newParent = node.left;
+                    node.left = null;
+                    newParent.left = node;
+                   newParent.parent = node.parent;
+                   node.parent = newParent;
+
+                   if(root == node){
+                       root = newParent;
+                   }
+
+                }
+                return;
+            }
+            return; // if we get here they both were null and we are a leaf
+        }
+        if(node.compareTo(node.left) < 0 || node.compareTo(node.right) < 0){ // assuming I got the < right we know a child is lower
+            if(node.left.compareTo(node.right) < 0){ //the right is smaller ? why didn't I make another custom method for this?
+                LinkedNode temp = node.right;
+
+            }
+            throw new UnsupportedOperationException();
+        }
     }
 
     @Override
@@ -83,25 +127,64 @@ public class LinkedMinHeap<T extends Comparable<T>> implements IMinHeap<T> {
             if(lastNodeChecked.left == null || lastNodeChecked.right == null){
                 return root; //we are adding to the root
             }
+            //we know we were brought all the way up time to go all the way down
+            return asFarLeft(root);
         }
         LinkedNode parent = lastNodeChecked.parent;
-        if(parent.right == lastNodeChecked){
+        if(parent.left == lastNodeChecked){
            return nodeToAddTo(parent);
     }
-throw new UnsupportedOperationException("I have no idea what I'm doing");
+return asFarLeft(parent.right);
 }
 private void bubbleUp(LinkedNode node){
         if(node == root){
             return;
         }
-        if(node.parent.compareTo(node) < 0 || node.parent.compareTo(node) > 0){
-       //todo make sure this is the right comparison
-            //todo swap children and make the node's parent it's grandparent and the old parents parent to be itself
+        if(node.parent.compareTo(node) > 0 ){ //parent bigger than child
 
-            throw new UnsupportedOperationException("Bubble up no workie yet");
-            //why am I getting an assertion error and not this one?
+            LinkedNode tempOldParent = node.parent;
+            if(tempOldParent.left == node){
+                //we know we were the left child
+                tempOldParent.left = node.left;
+                if(tempOldParent.left != null){
+                    tempOldParent.left.parent = tempOldParent;
+                }
+                LinkedNode tempChild = tempOldParent.right;
+                if(tempChild != null){
+                    tempChild.parent = node;
+                }
+                node.parent = tempOldParent.parent;
+                tempOldParent.parent = node;
+                node.left = tempOldParent;
+                if(tempOldParent == root){
+                    root = node;
+                }
+                return;
+            }
+            //we know we were the right child
+            tempOldParent.right = node.right;
+            if(tempOldParent.right != null){
+                tempOldParent.right.parent = tempOldParent;
+            }
+            LinkedNode tempChild = tempOldParent.left;
+            if(tempChild != null){
+            tempChild.parent = node;}
+            node.parent = tempOldParent.parent;
+            tempOldParent.parent = node;
+            node.right = tempOldParent;
+            if(tempOldParent == root){
+                root = node;
+            }
+
 
         }
 
+}
+
+private LinkedNode asFarLeft(LinkedNode node){
+        if(node.left == null){
+            return node;
+        }
+        return asFarLeft(node.left);
 }
 }
