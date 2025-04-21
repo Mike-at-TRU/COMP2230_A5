9c057083a2c6ec6eee0baeaac9a52d206daeb7e0
Michael Lebrun <Lebrunm231@mytru.ca>
2025-04-20 22:16:28 -0700

    new test to catch failed thing

--- src/test/java/MoreLinkedMinHeapTest.java
+++ src/test/java/MoreLinkedMinHeapTest.java
@@ -25,4 +25,29 @@ public class MoreLinkedMinHeapTest {
         }
     }
 
+    @Test
+    public void heapSortCopy() {
+        IMinHeap<Integer> h = new LinkedMinHeap<>();
+        h.add(9);
+        h.add(6);
+        h.add(8);
+        h.add(12);
+        h.add(7);
+        h.add(12);
+        h.add(3);
+        h.add(1);
+        h.add(7); // it seems the last number is never deleted
+        // when I print the tree the length doesn't change but it seems either the data
+        // is overwriten or it points to the last node added
+
+        assertEquals((Integer) 1, h.removeMin());
+        assertEquals((Integer) 3, h.removeMin());
+        assertEquals((Integer) 6, h.removeMin());
+        assertEquals((Integer) 7, h.removeMin());
+        assertEquals((Integer) 8, h.removeMin()); // 7!= 8
+        assertEquals((Integer) 9, h.removeMin());
+        assertEquals((Integer) 12, h.removeMin());
+
+    }
+
 }
